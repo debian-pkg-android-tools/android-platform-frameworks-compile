74894792624a5215a26c6fd1933876c5daf13ee0
Kai-Chung Yan <seamlikok@gmail.com>
2015-08-16 16:53:45 +0800

    Move exernal static libraries from "OBJECTS" to "build" prerequisites to avoid pattern unmatched error by "$(OBJECTS): %.o: %.cpp".

--- debian/libbcinfo.mk
+++ debian/libbcinfo.mk
@@ -6,18 +6,13 @@ NAME = libbcinfo
 SOURCES = BitcodeTranslator.cpp \
           BitcodeWrapper.cpp \
           MetadataExtractor.cpp
-OBJECTS = $(SOURCES:.cpp=.o) \
-          Wrap/libLLVMWrap.a \
-          BitReader_2_7/libLLVMBitReader_2_7.a \
-          BitReader_3_0/libLLVMBitReader_3_0.a \
-          ../../slang/BitWriter_3_2/libLLVMBitWriter_3_2.a \
-          /usr/lib/llvm-$(LLVM_VERSION)/lib/libLLVMCore.a
+OBJECTS = $(SOURCES:.cpp=.o)
 CXXFLAGS += -fPIC -c $(RS_VERSION_DEFINE)
 CPPFLAGS += $(ANDROID_INCLUDES) -I/usr/include/android -I../include -I../../slang
 LDFLAGS += -fPIC -shared -Wl,-rpath=/usr/lib/android -Wl,-soname,$(NAME).so.5 \
            -ldl -lpthread -L/usr/lib/android -lcutils -llog
 
-build: $(OBJECTS)
+build: $(OBJECTS) Wrap/libLLVMWrap.a BitReader_2_7/libLLVMBitReader_2_7.a BitReader_3_0/libLLVMBitReader_3_0.a ../../slang/BitWriter_3_2/libLLVMBitWriter_3_2.a
 	c++ $^ -o $(NAME).so.$(UPSTREAM_LIBVERSION) $(LDFLAGS)
 	ar rs $(NAME).a $(OBJECTS)
 	ln -s -f $(NAME).so.$(UPSTREAM_LIBVERSION) $(NAME).so
